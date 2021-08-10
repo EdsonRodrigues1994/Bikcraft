1bab5cdb8309682e37e7d27577481782076d5757
EdsonRodrigues1994 <edinho.1994@hotmail.com>
2021-08-09 21:40:08 -0300

    Inserindo animações com JS

--- web/js/script.js
+++ web/js/script.js
@@ -1,1 +1,17 @@
-new SimpleAnime();
+if (window.SimpleSlide){
+  new SimpleSlide({
+    slide:"quote",
+    time: 5000
+  });
+
+  new SimpleSlide({
+    slide:"portfolio",
+    nav: true,
+    time: 5000
+  });
+}
+
+if(window.SimpleAnime){
+  new SimpleAnime({
+  });
+}
